c8c94fa65af35ccd9929a5ca7f70242152f1d7ac
yasir.chu <yasir.chu@corp.to8to.com>
2021-02-05 17:06:44 +0800

    工具类的增加，旧代码无用代码的清理1

--- src/main/java/com/chuyx/controller/FileController.java
+++ src/main/java/com/chuyx/controller/FileController.java
@@ -8,7 +8,10 @@ import java.util.List;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,17 +19,18 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @Controller
+@Slf4j
 public class File {
-   @RequestMapping(
-      value = {"/file/upload"},
-      produces = {"application/json;charset=utf-8"}
-   )
+
+   @RequestMapping(value = {"/file/upload"}, produces = {"application/json;charset=utf-8"})
    @ResponseBody
    public String fileUpload(@RequestParam(value = "editormd-image-file",required = true) MultipartFile file, HttpServletRequest request) throws IOException, JSONException {
-      String filename = "chuyx-" + UUID.randomUUID().toString().replaceAll("-", "");
-      String s = UploadUtil.uploadQiniu(file);
+      String filename = UploadUtil.uploadQiniu(file);
+      if (StringUtils.isEmpty(filename)){
+         log.error("七牛云存储图片失败-");
+      }
       JSONObject res = new JSONObject();
-      res.put("url", "http://img.chuyx.top/" + s);
+      res.put("url", "http://img.chuyx.top/" + filename);
       res.put("success", 1);
       res.put("message", "upload success!");
       return res.toString();
---
+++ src/main/java/com/chuyx/utils/EmailUtils.java
@@ -0,0 +1,9 @@
+package com.chuyx.utils;
+/**
+ *
+ * @author yasir.chu
+ * @date 2021/2/5
+ **/
+ 
+public class EmailUtils {
+}
